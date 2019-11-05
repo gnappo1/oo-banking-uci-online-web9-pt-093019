a0bac389d46e53ed0286b36dec6c5e56f1243984
Matteo Piccini <gnappo.1@live.it>
2019-11-05 00:55:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,7 @@
 class Transfer
   attr_reader :amount, :sender, :receiver
+  attr_accessor :status
+  
   def initialize(sender, receiver, amount)
     @sender, @receiver, @amount, @status = sender, receiver, amount, "pending"
   end
