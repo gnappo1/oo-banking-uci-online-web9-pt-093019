211aa106f172bb65fff5caf2b41f9524a79f741c
Matteo Piccini <gnappo.1@live.it>
2019-11-05 01:00:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
     if valid? && sender.balance > amount
       sender.balance -= amount
       receiver.balance += amount
-      status = "completed"
+      self.status = "complete"
     else
       
     end
