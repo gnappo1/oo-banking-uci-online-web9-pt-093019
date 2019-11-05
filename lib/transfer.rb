6698271fbb9f98f374c95fb5e31b7bb939c76148
Matteo Piccini <gnappo.1@live.it>
2019-11-05 01:01:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
-      
+      "Transaction rejected. Please check your account balance"
     end
   end
   
