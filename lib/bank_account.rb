ca18186680ef6dad656b6e495d38c7fafecd8e57
Matteo Piccini <gnappo.1@live.it>
2019-11-05 00:56:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -11,6 +11,14 @@ class BankAccount
   end
   
   def display_balance
-    "Your balance is $#{balance}"
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    status == 'open' && balance > 0
+  end
+  
+  def close_account
+    self.status = 'closed'
   end
 end
